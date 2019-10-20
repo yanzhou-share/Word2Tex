175fc5f402e25e5b2fa968316568fbbb4d399065
yypai <45518828+yypai@users.noreply.github.com>
2019-10-19 23:11:59 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import textract
+import re
+
+text = textract.process("Word.docx")
+
+
+paragraph = text.decode()
+
+# replace all EndNote citation to RN
+citation = re.compile('[0-9a-zA-Z’]+,\s\w+\s#\w+')
+# citation.findall(paragraph)
+paragraph_mid = citation.sub(lambda m: 'RN'+ m.group().split('#')[-1], paragraph)
+
+
+citation_bracket = re.compile('\{\w+\;?\w+?\;?\w+?[0-9a-zA-Z;]+?\}')
+citation_bracket.findall(paragraph_mid)
+paragraph_end = citation_bracket.sub(lambda m: '\cite' + m.group().replace(';', ','), paragraph_mid)
+print(paragraph_end)
