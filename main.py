cbee8d53553316d0fe649dbedc3ae37a83465241
yypai <45518828+yypai@users.noreply.github.com>
2019-10-19 23:49:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,32 @@ import re
 
 text = textract.process("Word.docx")
 
-paragraph = text.decode()
+processed = text.decode()
 
 # replace all EndNote citation to RN
 citation = re.compile('[0-9a-zA-Z’]+,\s\w+\s#\w+')
-# citation.findall(paragraph)
-paragraph = citation.sub(lambda m: 'RN'+ m.group().split('#')[-1], paragraph)
+# citation.findall(processed)
+processed = citation.sub(lambda m: 'RN'+ m.group().split('#')[-1], processed)
 
 # add all citation with \cite{}
 citation_bracket = re.compile('\{\w+\;?\w+?\;?\w+?[0-9a-zA-Z;]+?\}')
-#citation_bracket.findall(paragraph)
-paragraph = citation_bracket.sub(lambda m: '\cite' + m.group().replace(';', ','), paragraph)
+#citation_bracket.findall(processed)
+processed = citation_bracket.sub(lambda m: '\cite' + m.group().replace(';', ','), processed)
 
-#print(paragraph)
+#print(processed)
+math = re.compile('[0-9a-zA-Z;,\(\)]+_[0-9a-zA-Z;\(\)]+\^[0-9a-zA-Z;\(\)]+')
+processed = math.sub(lambda m: '$' + m.group() + '$', processed)
 
+math = re.compile('[0-9a-zA-Z;,\(\)]+_[0-9a-zA-Z;\(\)]+[^\^]')
+processed = math.sub(lambda m: '$' + m.group() + '$', processed)
 
-math = re.compile('[0-9a-zA-Z;,\(\)]+_[0-9a-zA-Z;,\(\)]+\^?[0-9a-zA-Z;,\(\)\-]+?')
-#print(math.findall(paragraph))
-paragraph = math.sub(lambda m: '$' + m.group() + '$', paragraph)
-#print(paragraph)
+#print(processed)
 
+processed = processed.replace('SrTiO3', 'SrTiO$_3$').replace('LaAlO3', 'LaAlO$_3$')
+processed = processed.replace('CaTiO3', 'CaTiO$_3$')
+processed = processed.replace('~', '$\sim$')
 
-paragraph = paragraph.replace('SrTiO3', 'SrTiO$_3$').replace('LaAlO3', 'LaAlO$_3$')
-
+print(processed)
 
 with open("output.tex", "w") as file:
-    file.write(paragraph)
+    file.write(processed)
