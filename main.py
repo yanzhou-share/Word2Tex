ffeb25f8146285743763be270bd70a172aad270e
yypai <45518828+yypai@users.noreply.github.com>
2019-10-19 23:28:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-#Simple script converts word.docx manuscript to latex. 
-
 import textract
 import re
 
 text = textract.process("Word.docx")
-
 
 paragraph = text.decode()
 
 # replace all EndNote citation to RN
 citation = re.compile('[0-9a-zA-Z’]+,\s\w+\s#\w+')
 # citation.findall(paragraph)
-paragraph_mid = citation.sub(lambda m: 'RN'+ m.group().split('#')[-1], paragraph)
+paragraph = citation.sub(lambda m: 'RN'+ m.group().split('#')[-1], paragraph)
 
-
+# add all citation with \cite{}
 citation_bracket = re.compile('\{\w+\;?\w+?\;?\w+?[0-9a-zA-Z;]+?\}')
-citation_bracket.findall(paragraph_mid)
-paragraph_end = citation_bracket.sub(lambda m: '\cite' + m.group().replace(';', ','), paragraph_mid)
-print(paragraph_end)
+#citation_bracket.findall(paragraph)
+paragraph = citation_bracket.sub(lambda m: '\cite' + m.group().replace(';', ','), paragraph)
+
+#print(paragraph)
+
+
+math = re.compile('[0-9a-zA-Z;,\(\)]+_[0-9a-zA-Z;,\(\)]+\^?[0-9a-zA-Z;,\(\)\-]+?')
+#print(math.findall(paragraph))
+paragraph = math.sub(lambda m: '$' + m.group() + '$', paragraph)
+#print(paragraph)
+
+
+paragraph = paragraph.replace('SrTiO3', 'SrTiO$_3$').replace('LaAlO3', 'LaAlO$_3$')
+
+
+with open("output.tex", "w") as file:
+    file.write(paragraph)
